578d72feaa2b9f85d3f91baff84a485dc4a5fb55
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2022-09-11 14:35:12 +1000

    FORMAT test_securities.py - tidy test_securities file

--- tests/data/test_securities.py
+++ tests/data/test_securities.py
@@ -1,17 +1,12 @@
-
-
 import unittest
 import unittest.mock
-import pyasx.data
 import pyasx.data.securities
-import json
 
 
 class SecuritiesTest(unittest.TestCase):
     """
     Unit tests for pyasx.data.securities module
     """
-
 
     def __init__(self, *args, **kwargs):
         super(SecuritiesTest, self).__init__(*args, **kwargs)
@@ -20,20 +15,18 @@ class SecuritiesTest(unittest.TestCase):
         self.get_listed_securities_mock = ""
         self.get_security_info_mock = []
 
-
     def setUp(self):
 
         self.setUpGetListedSecurities()
         self.setUpGetSecurityInfo()
 
-
     def setUpGetListedSecurities(self):  # TODO
 
         self.get_listed_securities_data = [
             # just the first ones from the file to test with
-            [ "IJH", "ISHARES MID-CAP ETF", "CHESS DEPOSITARY INTERESTS 1:1 ISHS&P400", "AU000000IJH2" ],
-            [ "MOQ", "MOQ LIMITED", "ORDINARY FULLY PAID", "AU000000MOQ5" ],
-            [ "MOQAI", "MOQ LIMITED", "OPTION EXPIRING VARIOUS DATES EX VARIOUS PRICES", "AU0000MOQAI6" ],
+            ["IJH", "ISHARES MID-CAP ETF", "CHESS DEPOSITARY INTERESTS 1:1 ISHS&P400", "AU000000IJH2"],
+            ["MOQ", "MOQ LIMITED", "ORDINARY FULLY PAID", "AU000000MOQ5"],
+            ["MOQAI", "MOQ LIMITED", "OPTION EXPIRING VARIOUS DATES EX VARIOUS PRICES", "AU0000MOQAI6"],
         ]
 
         # build the mock CSV data based on self.get_listed_companies_data
@@ -42,12 +35,10 @@ class SecuritiesTest(unittest.TestCase):
             self.get_listed_securities_mock += "HEADER\tROW\n"
 
         for row in self.get_listed_securities_data:
-
             csv_row = "\t".join(row)
             csv_row += "\n"
 
             self.get_listed_securities_mock += csv_row
-
 
     def setUpGetSecurityInfo(self):
 
@@ -86,27 +77,25 @@ class SecuritiesTest(unittest.TestCase):
             "deprecated_number_of_shares": 25,
             "suspended": False,
             "status": [
-              "CD"
+                "CD"
             ],
             "indices": [
                 {
-                  "index_code": "XJO",
-                  "name_full": "S&P/ASX 200",
-                  "name_short": "S&P/ASX200",
-                  "name_abrev": "S&P/ASX 200"
+                    "index_code": "XJO",
+                    "name_full": "S&P/ASX 200",
+                    "name_short": "S&P/ASX200",
+                    "name_abrev": "S&P/ASX 200"
                 }
             ]
         }
 
-
-    def testGetListedSecuritiesMocked(self):
+    def test_get_listed_securities_live_mocked(self):
         """
         Unit test for pyasx.data.securities.get_listed_securities()
         Test pulling mock data + verify
         """
 
         with unittest.mock.patch("requests.get") as mock:
-
             # set up mock iterator for response.iter_content()
 
             bytes_mock = bytes(self.get_listed_securities_mock, "utf-8")
@@ -118,7 +107,7 @@ class SecuritiesTest(unittest.TestCase):
             securities = pyasx.data.securities.get_listed_securities()
 
             # verify data is all correct
-            i = 0;
+            i = 0
             for security in securities:
                 security_data = self.get_listed_securities_data[i]
 
@@ -129,25 +118,22 @@ class SecuritiesTest(unittest.TestCase):
 
                 i += 1
 
-
-    def testGetListedSecuritiesLive(self):
+    def test_get_listed_securities_live(self):
         """
         Unit test for pyasx.data.securities.get_listed_securities()
         Simple check of pulling live data
         """
 
         securities = pyasx.data.securities.get_listed_securities()
-        self.assertTrue(len(securities) > 1000) # there are at least a few thousand listed securities
+        self.assertTrue(len(securities) > 1000)  # there are at least a few thousand listed securities
 
-
-    def testGetSecurityInfoMocked(self):
+    def test_get_security_info_mocked(self):
         """
         Unit test for pyasx.data.securities.get_security_info()
         Test pulling mock data + verify
         """
 
         with unittest.mock.patch("requests.get") as mock:
-
             # set up mock iterator for response.json()
             instance = mock.return_value
             instance.json.return_value = self.get_security_info_mock
@@ -200,8 +186,7 @@ class SecuritiesTest(unittest.TestCase):
             self.assertEqual(security["is_suspended"], False)
             self.assertTrue(len(security["indices"]))
 
-
-    def testGetSecurityInfoLive(self):
+    def test_get_security_info_live(self):
         """
         Unit test for pyasx.data.securities.get_security_info()
         Simple check of pulling live data
