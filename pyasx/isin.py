47d562195ad50bcbb1e2cd21b01c4fa777b6bc52
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2022-09-12 21:47:20 +1000

    CREATE isin.py - add the ISIN class from another repo

---
+++ pyasx/isin.py
@@ -0,0 +1,105 @@
+"""
+The ISIN class provides all the functionality required to work with ISIN
+codes at a basic level.
+
+Reference on the format of valid ISINs:
+    https://www.isin.org/isin-format/
+"""
+
+
+class ISIN(object):
+    """
+    Represents an ISIN code.
+
+    The structure of an ISIN is composed of three components:
+
+        1. A two-letter country code
+        2. A nine-character alphanumeric security identifier
+        3. A single check digit.
+    """
+    def __init__(self, *, isin: str = None):
+        """
+
+        Args:
+            isin: str
+                An ISIN code.
+
+        Raises:
+            ValueError: On ISIN inputs that are not 12 characters long.
+        """
+        self._isin: str = isin
+        self._security_identifier: str = self._parse_security_identifier()
+        self._country_code: str = self._parse_country()
+        self._check_digit: str = self._parse_check_digit()
+
+        isin_length = len(isin)
+        if isin_length != 12:
+            raise ValueError(f'ISIN codes must always be 12 characters, not {isin_length}')
+
+    def __str__(self):
+        return f'ISIN={self._isin}'
+
+    def __repr__(self):
+        # automatically get name to minimise refactor touch points
+        return f'{self.__class__.__name__}(isin={self._isin})'
+
+    @property
+    def country_code(self) -> str:
+        return self._country_code
+
+    @property
+    def check_digit(self) -> str:
+        return self._check_digit
+
+    @property
+    def security_identifier(self) -> str:
+        return self._security_identifier
+
+    def _parse_country(self) -> str:
+        """
+        As per the ISIN format documentation listed in the module docstring,
+        the 2-letter country code will always be found in the first two chars
+        of the ISIN code.
+
+        https://www.isin.org/country-codes/
+
+        Returns:
+            str: The 2-letter country code of the ISIN
+
+        """
+        country_code: str = self._isin[0:2]
+        assert country_code.isalpha()
+        return country_code
+
+    def _parse_check_digit(self) -> str:
+        """
+        The check_digit is used for validation via the Luhn algorithm.
+        https://en.wikipedia.org/wiki/Luhn_algorithm
+
+        Returns:
+            str: the check_digit
+        """
+        check_digit: str = self._isin[-1]
+        assert check_digit.isnumeric()
+        return check_digit
+
+    def _parse_security_identifier(self) -> str:
+        security_identifier: str = self._isin[2:11]
+        assert security_identifier.isalnum()
+        return security_identifier
+
+    # def _compute_check(self) -> str:
+    #     """
+    #     The check is performed using the Luhn algorithm
+    #     https://en.wikipedia.org/wiki/Luhn_algorithm
+    #
+    #     # TODO: add ability to compute the check, will be available after
+    #     Returns:
+    #         str
+    #     Raises
+    #         NotImplementedError
+    #     """
+    #
+    #     for character in list(self._country_code):
+    #         value = ord(character)
+    #         print(value)
