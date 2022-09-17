43560020a2550c8323b8cbb98c9a58d4f1376f10
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2022-09-18 00:39:07 +1000

    UPDATE luhn.py - add __repr__ method on Luhn class

--- pyasx/helpers/luhn.py
+++ pyasx/helpers/luhn.py
@@ -5,12 +5,18 @@ class Luhn(object):
         self._check_digit: int = self._get_check_digit()
         self._valid: bool = False
 
+    def __repr__(self) -> str:
+        return f'Luhn(checksum={self._checksum})'
+
     def _get_check_digit(self) -> int:
+        print(self._checksum)
         check_digit: int = int(self._checksum[-1])
+        print(check_digit)
         return check_digit
 
     @staticmethod
     def transpose_isin_to_luhn_checksum(*, isin: str = None) -> str:
+        country_code = isin[0:1]
+        return country_code
+
         raise NotImplementedError
-
-
