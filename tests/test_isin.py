f90dd78649b10650f9be645e870e480c836af3fb
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2022-09-20 21:48:10 +1000

    UPDATE test_isin.py - utilise pytest fixtures for isin initialisation

--- tests/test_isin.py
+++ tests/test_isin.py
@@ -3,19 +3,26 @@ import pytest
 from pyasx.isin import ISIN
 
 
-def test_country_code():
+@pytest.fixture
+def valid_isin():
     isin = ISIN(isin='AU0000068413')
-    assert isin.country_code == 'AU'
+    return isin
+
+@pytest.fixture
+def invalid_isin():
+    isin = ISIN(isin='AU0000068234')
+    return isin
+
+def test_country_code(valid_isin):
+    assert valid_isin.country_code == 'AU'
 
 
-def test_check_digit():
-    isin = ISIN(isin='AU0000068413')
-    assert isin.check_digit == '3'
+def test_check_digit(valid_isin):
+    assert valid_isin.check_digit == '3'
 
 
-def test_security_identifier():
-    isin = ISIN(isin='AU0000068413')
-    assert isin.security_identifier == '000006841'
+def test_security_identifier(valid_isin):
+    assert valid_isin.security_identifier == '000006841'
 
 
 def test_isin_length_validation():
@@ -23,21 +30,17 @@ def test_isin_length_validation():
         ISIN(isin='XX012345678')
 
 
-def test_isin_string_representation():
-    isin = ISIN(isin='AU0000068413')
-    assert str(repr(isin)) == 'ISIN(isin=AU0000068413)'
+def test_isin_string_representation(valid_isin):
+    assert str(repr(valid_isin)) == 'ISIN(isin=AU0000068413)'
 
 
-def test_isin_print_representation():
-    isin = ISIN(isin='AU0000068413')
-    assert str(isin) == 'ISIN=AU0000068413'
+def test_isin_print_representation(valid_isin):
+    assert str(valid_isin) == 'ISIN=AU0000068413'
 
 
-def test_isin_successful_validation():
-    isin = ISIN(isin='AU0000068413')
-    assert isin.validate() is True
+def test_isin_successful_validation(valid_isin):
+    assert valid_isin.validate() is True
 
 
-def test_isin_failed_validation():
-    isin = ISIN(isin='AU0000068418')
-    assert isin.validate() is False
+def test_isin_failed_validation(invalid_isin):
+    assert invalid_isin.validate() is False
