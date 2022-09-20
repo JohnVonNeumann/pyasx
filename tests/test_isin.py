41f74900dc6237a0aff9e2e60484e585f17480b6
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2022-09-20 20:13:55 +1000

    FORMAT test_isin.py - migrate unittest focused approach to pytest

--- tests/test_isin.py
+++ tests/test_isin.py
@@ -1,37 +1,43 @@
-import unittest
+import pytest
 
 from pyasx.isin import ISIN
 
 
-class ISINTestCase(unittest.TestCase):
-    def test_country_code(self):
-        isin = ISIN(isin='AU0000068413')
-        self.assertEqual(isin.country_code, 'AU')
+def test_country_code():
+    isin = ISIN(isin='AU0000068413')
+    assert isin.country_code == 'AU'
 
-    def test_check_digit(self):
-        isin = ISIN(isin='AU0000068413')
-        self.assertEqual(isin.check_digit, '3')
 
-    def test_security_identifier(self):
-        isin = ISIN(isin='AU0000068413')
-        self.assertEqual(isin.security_identifier, '000006841')
+def test_check_digit():
+    isin = ISIN(isin='AU0000068413')
+    assert isin.check_digit == '3'
 
-    def test_isin_length_validation(self):
-        with self.assertRaises(ValueError):
-            isin = ISIN(isin='XX012345678')
 
-    def test_isin_string_representation(self):
-        isin = ISIN(isin='AU0000068413')
-        self.assertEqual(str(repr(isin)), 'ISIN(isin=AU0000068413)')
+def test_security_identifier():
+    isin = ISIN(isin='AU0000068413')
+    assert isin.security_identifier == '000006841'
 
-    def test_isin_print_representation(self):
-        isin = ISIN(isin='AU0000068413')
-        self.assertEqual(str(isin), 'ISIN=AU0000068413')
 
-    def test_isin_successful_validation(self):
-        isin = ISIN(isin='AU0000068413')
-        assert isin.validate() is True
+def test_isin_length_validation():
+    with pytest.raises(ValueError):
+        ISIN(isin='XX012345678')
 
-    def test_isin_failed_validation(self):
-        isin = ISIN(isin='AU0000068418')
-        assert isin.validate() is False
+
+def test_isin_string_representation():
+    isin = ISIN(isin='AU0000068413')
+    assert str(repr(isin)) == 'ISIN(isin=AU0000068413)'
+
+
+def test_isin_print_representation():
+    isin = ISIN(isin='AU0000068413')
+    assert str(isin) == 'ISIN=AU0000068413'
+
+
+def test_isin_successful_validation():
+    isin = ISIN(isin='AU0000068413')
+    assert isin.validate() is True
+
+
+def test_isin_failed_validation():
+    isin = ISIN(isin='AU0000068418')
+    assert isin.validate() is False
