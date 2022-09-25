07dd6b08c86f08a70f30bbce0d2bdb30d9ab013a
JohnVonNeumann <18162779+JohnVonNeumann@users.noreply.github.com>
2022-09-25 19:01:25 +1000

    CREATE test_security.py - add security tests for the initial properties

---
+++ tests/test_security.py
@@ -0,0 +1,33 @@
+import pytest
+
+from pyasx.security import Security
+from pyasx.isin import ISIN
+
+
+@pytest.fixture
+def valid_security() -> Security:
+    security = Security(
+        security_type='TEST ORDINARY SECURITY',
+        isin=ISIN(
+            isin='AU000000TES7'
+        ),
+        ticker='TES',
+        company_name='TEST SECURITY'
+    )
+    return security
+
+
+def test_security_security_type(valid_security):
+    assert valid_security.security_type == 'TEST ORDINARY SECURITY'
+
+
+def test_security_isin(valid_security):
+    assert valid_security.isin == 'ISIN=AU000000TES7'
+
+
+def test_security_ticker(valid_security):
+    assert valid_security.ticker == 'TES'
+
+
+def test_security_company_name(valid_security):
+    assert valid_security.company_name == 'TEST SECURITY'
